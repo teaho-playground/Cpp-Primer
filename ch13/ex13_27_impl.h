e802013e45e71bc87a8103451d7bb74b21d56ab0
teaho2015 <teaho2015@gmail.com>
2024-05-27 21:20:15 +0800

    add

--- ch13/ex13_27_impl.h
+++ ch13/ex13_27_impl.h
@@ -11,7 +11,16 @@ class HasPtr {
 
     public:
         HasPtr(const std::string &s = std::string()) : ps(new std::string()),i(0),use(new size_t(1)) {}
+        HasPtr(const HasPtr &hp) : ps(hp.ps), i(hp.i), use(hp.use) {}
 
+        HasPtr& operator=(const HasPtr &hs) {
+            ++*hs.use;
+            if (*use == 0) {
+                /* code */
+            }
+            
+
+        }
 
     private:
         std::string *ps;
