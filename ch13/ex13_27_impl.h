8e31ba71f95bb84f77904c39adca71e35d80b2fd
teaho2015 <teaho2015@gmail.com>
2024-05-28 10:50:06 +0800

    13.27

--- ch13/ex13_27_impl.h
+++ ch13/ex13_27_impl.h
@@ -10,16 +10,32 @@
 class HasPtr {
 
     public:
-        HasPtr(const std::string &s = std::string()) : ps(new std::string()),i(0),use(new size_t(1)) {}
+        HasPtr(const std::string &s = std::string()) : ps(new std::string(s)),i(0),use(new size_t(1)) {}
         HasPtr(const HasPtr &hp) : ps(hp.ps), i(hp.i), use(hp.use) {}
 
         HasPtr& operator=(const HasPtr &hs) {
             ++*hs.use;
-            if (*use == 0) {
-                /* code */
+            if (--*use == 0) {
+                delete ps;
+                delete use;
             }
+            ps = hs.ps;
+            i = hs.i;
+            use = hs.use;
+            return *this;
             
 
+        }
+
+        ~HasPtr() {
+            if(--*use == 0) {
+                delete ps;
+                delete use;
+            }
+        }
+
+        auto getPs() {
+            return *ps;
         }
 
     private:
---
+++ ch13/ex13_27_run.cpp
@@ -0,0 +1,21 @@
+/**
+ * @author teaho2015@gmail.com
+ * @date 2024-05
+*/
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+#include "ex13_27_impl.h"
+
+
+using std::cout, std::endl, std::cin;
+
+
+
+int main()
+{
+
+    HasPtr hp("hello");
+    cout << hp.getPs() << endl;
+
+}
